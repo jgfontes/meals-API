2b93a0bd2da3c505b0e6cd688eeae73c7337d9ba
Julio Conta Pessoal <ju.1996@outlook.com>
2023-05-07 16:13:38 -0300

    Thymeleaf added to the project, HTML draft added. Still not working and needs to be fixed.

--- src/main/java/com/jgfontes/Mealsproject/Controller/MealsController.java
+++ src/main/java/com/jgfontes/Mealsproject/Controller/MealsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Controls requests comming directly from API calls
 @RestController
 @RequestMapping("/meals")
 @RequiredArgsConstructor
